0367370f5556ac60458dc0de64717eab3713e583
heetacharya12 <heetacharya12@gmail.com>
2025-10-07 22:58:17 +0530

    Add files via upload

---
+++ 6-10/main (4).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int x = 10;
+        
+    while(x>=1)
+    {
+        printf("\t %d",x);
+        x--;
+    }
+}
---
+++ 6-10/main (5).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    
+    int x=1;
+    
+    do
+    {
+        printf("\t %d",x);
+        x++;
+    }
+    while(x<=10);
+}
